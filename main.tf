107e80c76b984b7c1ff42f0118a167a12e9df22e
naresh <Naresh@gmail.com>
2025-04-11 20:51:21 +0530

    terr

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "null_resource" "expenseApp" {
     type     = "ssh"
     user     = var.user
     password = var.password
-    host     = data.aws_instance.main[each.key].private_ip
+    host     = data.aws_instance.main.private_ip
   }
    provisioner "remote-exec" {
     inline = [
