7f93400d8894aa8a60c8d239575d9b0dce2b00e5
naresh <Naresh@gmail.com>
2025-04-11 17:25:12 +0530

    terr

--- main.tf
+++ main.tf
@@ -10,29 +10,11 @@ module "app"{
     env = var.env
 
 }
-# module "mysql"{
-#     source = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
-#     ami = var.ami
-#     vpc_security_group_ids = var.vpc_security_group_ids
-#     Name = "mysql"
-#     zone_id = var.zone_id
-#     domain = var.domain
-#     instance_type = var.instance_type
 
-# }
-# module "backend"{
-#     source = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
-#     ami = var.ami
-#     vpc_security_group_ids = var.vpc_security_group_ids
-#     Name = "backend"
-#     zone_id = var.zone_id
-#     domain = var.domain
-#     instance_type = var.instance_type
-# }
-# resource "null_resource" "expenseApp" {
-#     provisioner "local-exec" {
-#     command = "sleep 10; cd /home/ec2-user/ansible_Expense_Roles ; ansible-playbook -i inv-${var.env} -e ansible_username=ec2-user -e ansible_password=DevOps321 -e env=dev -e component=${var.Name} expense.yml"
+resource "null_resource" "expenseApp" {
+    provisioner "local-exec" {
+    command = "sleep 10; cd /home/ec2-user/ansible_Expense_Roles ; ansible-playbook -i inv-${var.env} -e ansible_username=ec2-user -e ansible_password=DevOps321 -e env=dev -e component=${var.components[each.value["Name"]]} expense.yml"
     
-#     }
+    }
   
-# }
+}
