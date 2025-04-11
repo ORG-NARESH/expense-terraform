89cb42fbc824dc0a3d7c72b386bf2f7427d0654d
naresh <Naresh@gmail.com>
2025-04-11 22:13:35 +0530

    terr

--- data.tf
+++ data.tf
@@ -1,10 +1,11 @@
 data "aws_instance" "main" {
-    #for_each = var.components
-    filter {
-    name   = "tag:Name"
-    values = [each.value["Name"]]
+   instance_id = data.aws_instance.main.instance_id
   }
-}
+
+# data "aws_instance" "main" {
+#    instance_id = data.aws_instance.main.instance_id
+#   }
+
 
 # output "instance" {
 #     value = data.aws_instance.main.private_ip
