5aadbbf0ef98091925e644e5a5d51224fa02fb8c
naresh <Naresh@gmail.com>
2025-04-11 12:39:28 +0530

    terr

--- dev/state.tf
+++ dev/state.tf
@@ -1,6 +1,7 @@
-backend "s3" {
+terraform {
+  backend "s3" {
     bucket         	   = "b59-terraformstate"
     key              	   = "dev/state.tf"
     region         	   = "us-east-1"
-    
   }
+}
