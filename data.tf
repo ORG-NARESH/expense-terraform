b5be10ad0ef71bf709d9ab259a8ab04a5ef37e70
naresh <Naresh@gmail.com>
2025-04-12 07:59:26 +0530

    terr

--- data.tf
+++ data.tf
@@ -1,16 +1,3 @@
-# data "aws_instance" "main" {
-#    instance_id = data.aws_instance.main.instance_id
-#   }
-
-# data "aws_instance" "main" {
-#    instance_id = data.aws_instance.main.instance_id
-#   }
-
-
-# output "instance" {
-#     value = data.aws_instance.main.private_ip
-# }
-
-# data "aws_instance" "main" {
-#     private_ip = aws_instance.main.private_ip
-# }
+data "app"  app1 {
+    ip = module.app.private_ip
+}
