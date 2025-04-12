92d220b61d9ba9e5d52ebc9c98f6ad29034bb9ec
naresh <Naresh@gmail.com>
2025-04-12 09:13:22 +0530

    terr

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
-data "aws_instance" "main" {
-    private_ip = data.aws_instance.main.private_ip
-}
+# data "aws_instance" "main" {
+#     private_ip = data.aws_instance.main.private_ip
+# }
 
-output "private_ip" {
-    value = data.aws_instance.main.private_ip
-}
+# output "private_ip" {
+#     value = data.aws_instance.main.private_ip
+# }
