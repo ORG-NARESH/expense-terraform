2fd550abb8e10ee8639328b67af67fc8ecf833f0
naresh <Naresh@gmail.com>
2025-04-14 16:57:57 +0530

    terraform expense config

--- data.tf
+++ data.tf
@@ -17,11 +17,7 @@ data "aws_ami" "main" {
 # }
 
 data "aws_security_group" "main" {
-  filter {
-    name = "Name"
-    values = var.security_group_name
-  }
-
+  name = var.security_group_name
 }
 
 data "aws_route53_zone" "main" {
