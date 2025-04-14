870b6831bd445710dd9a9b35cf4d3dce0a0d745d
naresh <Naresh@gmail.com>
2025-04-14 22:29:14 +0530

    terraform expense config

--- dev/state.tf
+++ dev/state.tf
@@ -1,5 +1,5 @@
- bucket         	   = "b59terraformstate2"
-key              	   = "devv/state.tf"
-region         	   =  "us-east-1"
+#  bucket         	   = "b59terraformstate2"
+# key              	   = "devv/state.tf"
+# region         	   =  "us-east-1"
   
 
