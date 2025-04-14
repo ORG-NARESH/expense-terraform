dc48785aefdf4b4ca4b03fbd663da1cbf8b65575
naresh <Naresh@gmail.com>
2025-04-14 17:04:02 +0530

    terraform expense config

--- data.tf
+++ data.tf
@@ -17,7 +17,7 @@ data "aws_ami" "main" {
 # }
 
 data "aws_security_group" "main" {
-  name = [var.security_group_name]                                  
+  name = ["B59-Terraform-SG"]                             
 }
 
 data "aws_route53_zone" "main" {
