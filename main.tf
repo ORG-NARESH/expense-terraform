f41912f1f89bf3039c62cd0e75cfbeeb59c87816
naresh <Naresh@gmail.com>
2025-04-14 21:30:36 +0530

    terraform expense config

--- main.tf
+++ main.tf
@@ -1,19 +1,46 @@
-module "app"{
-    source = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
-    for_each = var.components
-    ami = data.aws_ami.main.id
-    vpc_security_group_ids = var.vpc_security_group_ids
-    Name = each.value["Name"]
-    zone_id = data.aws_route53_zone.main.zone_id
-    domain = var.domain
-    instance_type = each.value["instance_type"]
-    env = var.env
-    #port_no = each.value["port_no"]
+# module "app"{
+#     source = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
+#     for_each = var.components
+#     ami = data.aws_ami.main.id
+#     vpc_security_group_ids = var.vpc_security_group_ids
+#     Name = each.value["Name"]
+#     zone_id = data.aws_route53_zone.main.zone_id
+#     domain = var.domain
+#     instance_type = each.value["instance_type"]
+#     env = var.env
+#     #port_no = each.value["port_no"]
 
+# }
+
+module "mysql" {
+  source = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
+  ami = data.aws_ami.main.id
+  zone_id = data.aws_route53_zone.main.zone_id
+  domain = var.domain
+  
 }
 
+module "backend" {
+  depends_on = [ module.mysql ]
+  source = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
+  ami = data.aws_ami.main.id
+  zone_id = data.aws_route53_zone.main.zone_id
+  domain = var.domain
+  
+}
+
+module "frontend" {
+  depends_on = [ module.backend ]
+  source = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
+  ami = data.aws_ami.main.id
+  zone_id = data.aws_route53_zone.main.zone_id
+  domain = var.domain
+  
+}
+
+
+
 resource "null_resource" "expenseApp" {
-  depends_on = [ module.app ]
     for_each = var.components
     connection {
     type     = "ssh"
@@ -26,7 +53,7 @@ resource "null_resource" "expenseApp" {
     inline = [
       "sleep 10",
       "pip3.11 install ansible",
-      "ansible-pull -U https://github.com/ORG-NARESH/ansible_Expense_Roles.git -e env=dev -e component=${each.value["Name"]} Roles/expense-pull.yml"
+      "ansible-pull -U https://github.com/ORG-NARESH/ansible_Expense_Roles.git -e env=${var.env} -e component=${each.value["Name"]} Roles/expense-pull.yml"
     ]
   }
   
