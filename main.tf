c28823c59a96be8ef245318d20b3392d4d0dff52
naresh <Naresh@gmail.com>
2025-04-15 21:20:05 +0530

    terraform expense config

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "mysql" {
   ami = data.aws_ami.main.id
   zone_id = data.aws_route53_zone.main.zone_id
   domain = var.domain
-  vpc_security_group_ids  = var.components["mysql"].vpc_security_group_ids
+  vpc_security_group_ids  = ""
   Name = "mysql"
   env = var.env
   instance_type = ""
@@ -17,7 +17,7 @@ module "backend" {
   ami = data.aws_ami.main.id
   zone_id = data.aws_route53_zone.main.zone_id
   domain = var.domain
-  vpc_security_group_ids = var.components["backend"].vpc_security_group_ids
+  vpc_security_group_ids = ""
   Name = "backend"
   env = var.env
   instance_type = ""
@@ -30,7 +30,7 @@ module "frontend" {
   ami = data.aws_ami.main.id
   zone_id = data.aws_route53_zone.main.zone_id
   domain = var.domain
-  vpc_security_group_ids = var.components["frontend"].vpc_security_group_ids
+  vpc_security_group_ids = ""
   Name = "frontend"
   env = var.env
   instance_type = ""
