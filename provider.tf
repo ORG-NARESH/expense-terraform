01325e4a6676540b64d11def74d564d4da016cc1
naresh <Naresh@gmail.com>
2025-04-16 22:28:39 +0530

    jenkins config

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aws" {}
 
 terraform {
   backend "s3" {
-     bucket         	   = "b59terraformstate2"
+    bucket         	   = "b59terraformstate2"
     key              	   = "that/state.tf"
     region         	   = "us-east-1"
   }
