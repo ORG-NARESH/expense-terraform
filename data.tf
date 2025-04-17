818f4b6878960d693cb0aef1e0c1725442928fcc
naresh <Naresh@gmail.com>
2025-04-17 20:47:43 +0530

    jenkins config

--- data.tf
+++ data.tf
@@ -1,39 +1,39 @@
-data "aws_ami" "main" {
+# data "aws_ami" "main" {
  
-  most_recent      = true
-  owners           = ["355449129696"]
+#   most_recent      = true
+#   owners           = ["355449129696"]
   
- filter {
-    name   = "description"
-    values = ["DevOps-LabImage-RHEL9"]
-  }
+#  filter {
+#     name   = "description"
+#     values = ["DevOps-LabImage-RHEL9"]
+#   }
 
 
-}
+# }
 
 
-# output "verifyingDns" {
-#   value = data.aws_security_group.main.id
+# # output "verifyingDns" {
+# #   value = data.aws_security_group.main.id
+# # }
+
+# # data "aws_security_group" "main" {
+
+# #   name = var.security_group_name                       
+# # }
+
+# data "aws_route53_zone" "main" {
+#   name         = var.domain
 # }
 
 # data "aws_security_group" "main" {
-
-#   name = var.security_group_name                       
+#   id = data.aws_security_group.main.id
 # }
 
-data "aws_route53_zone" "main" {
-  name         = var.domain
-}
-
-data "aws_security_group" "main" {
-  id = data.aws_security_group.main.id
-}
-
-# data "aws_route53_zone" "main1" {
-#  zone_id = data.aws_route53_record.main.zone_id
-# } 
-# data "aws_route53_zone" "main" {
-#  zone_id = data.aws_route53_zone.main.zone_id
+# # data "aws_route53_zone" "main1" {
+# #  zone_id = data.aws_route53_record.main.zone_id
+# # } 
+# # data "aws_route53_zone" "main" {
+# #  zone_id = data.aws_route53_zone.main.zone_id
   
-# }
+# # }
 
