e2e7ff459f503dfb86ab85eda51417c4bf733fd3
naresh <Naresh@gmail.com>
2025-04-19 11:23:02 +0530

    jenkins config

--- variables.tf
+++ variables.tf
@@ -24,21 +24,21 @@ variable "components" {
         Name = "mysql"
         #port_no = "3306"
         instance_type = "t3.micro"
-        vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+        #vpc_security_group_ids = ["sg-02efeff1df99019a6"]
     }
     backend = {
        
         Name = "backend"
         #port_no = "8080"
         instance_type = "t2.micro"
-        vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+        #vpc_security_group_ids = ["sg-02efeff1df99019a6"]
     }
     frontend = {
         
         Name = "frontend"
         #port_no = "80"
         instance_type = "t2.small"
-        vpc_security_group_ids = ["sg-02efeff1df99019a6"]
+        #vpc_security_group_ids = ["sg-02efeff1df99019a6"]
     }
   
 }
