b3c2dc4eb9dafe4df524ddc78576ba22231bb883
naresh <Naresh@gmail.com>
2025-04-19 18:01:51 +0530

    jenkins automation

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 provider "aws" {}
 
-# terraform {
-#   backend "s3" {
-#     bucket         	   = "b59terraformstate2"
-#     key              	   = "that/state.tf"
-#     region         	   = "us-east-1"
-#   }
+terraform {
+  backend "s3" {
+    bucket         	   = "b59terraformstate2"
+    key              	   = "that/state.tf"
+    region         	   = "us-east-1"
+  }
 
-# }
+}
