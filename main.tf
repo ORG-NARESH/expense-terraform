d4b3679110a1c05843c3ae410a85fc4301a7d448
naresh <Naresh@gmail.com>
2025-04-25 10:43:10 +0530

    jenkins automation

--- main.tf
+++ main.tf
@@ -55,6 +55,7 @@ resource "null_resource" "expenseApp" {
    provisioner "remote-exec" {
     inline = [
       "sleep 10",
+      "pip3.11 install ansible",
       "ansible-pull -U https://github.com/ORG-NARESH/ansible_Expense_Roles.git -e env=${var.env} -e component=${each.value["Name"]} Roles/expense-pull.yml"
     ]
   }
