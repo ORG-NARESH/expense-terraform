c879f35c01d0b5ec6df6185c68cc78f7b1079271
naresh <Naresh@gmail.com>
2025-04-25 11:31:54 +0530

    jenkins automation

--- data.tf
+++ data.tf
@@ -1,15 +1,15 @@
-# data "aws_ami" "main" {
+data "aws_ami" "main" {
 
-#   most_recent = true
-#   owners      = ["703671922956"]
+  most_recent = true
+  owners      = ["355449129696"]
 
-#   filter {
-#     name   = "name"
-#     values = ["DevOps-LabImage-RHEL9*"]
-#   }
+  filter {
+    name   = "description"
+    values = ["DevOps-LabImage-RHEL9*"]
+  }
 
 
-# }
+}
 
 
 data "aws_route53_zone" "main" {
