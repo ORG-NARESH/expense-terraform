5c664e6356bbffc798d213f8d82e5028bd2a7e3f
naresh <Naresh@gmail.com>
2025-04-26 20:03:01 +0530

    jenkins automation

--- dev/dev.tfvars
+++ dev/dev.tfvars
@@ -3,35 +3,35 @@ env = "dev"
 components = {
 
   mysql = {
-    Name                    = "mysql"
-    port_no                 = "3306"
-    instance_type           = "t3.medium"
-    port_no_of_nodeexporter = "9100"
+    Name          = "mysql"
+    port_no       = "3306"
+    instance_type = "t3.medium"
+    #port_no_of_nodeexporter = "9100"
 
   }
   backend = {
 
-    Name                    = "backend"
-    port_no                 = "8080"
-    instance_type           = "t3.micro"
-    port_no_of_nodeexporter = "9100"
+    Name          = "backend"
+    port_no       = "8080"
+    instance_type = "t3.micro"
+    #port_no_of_nodeexporter = "9100"
 
   }
   frontend = {
 
-    Name                    = "frontend"
-    port_no                 = "80"
-    instance_type           = "t3.small"
-    port_no_of_nodeexporter = "9100"
+    Name          = "frontend"
+    port_no       = "80"
+    instance_type = "t3.small"
+    #port_no_of_nodeexporter = "9100"
 
   }
-  node123 = {
+  # node123 = {
 
-    Name                    = "node123"
-    port_no                 = "80"
-    instance_type           = "t2.small"
-    port_no_of_nodeexporter = "9100"
+  #   Name                    = "node123"
+  #   port_no                 = "80"
+  #   instance_type           = "t2.small"
+  #   #port_no_of_nodeexporter = "9100"
 
-  }
+  # }
 
 }
--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ module "mysql" {
   zone_id = data.aws_route53_zone.main.zone_id
   domain  = var.domain
   #vpc_security_group_ids  = [aws_security_group.main.id]  
-  Name                    = "mysql"
-  env                     = var.env
-  instance_type           = var.components["mysql"].instance_type
-  port_no                 = var.components["mysql"].port_no
-  port_no_of_nodeexporter = var.components["mysql"].port_no_of_nodeexporter
+  Name          = "mysql"
+  env           = var.env
+  instance_type = var.components["mysql"].instance_type
+  port_no       = var.components["mysql"].port_no
+  #port_no_of_nodeexporter = var.components["mysql"].port_no_of_nodeexporter
 
 
 }
@@ -20,11 +20,11 @@ module "backend" {
   zone_id    = data.aws_route53_zone.main.zone_id
   domain     = var.domain
   #vpc_security_group_ids =  var.vpc_security_group_ids
-  Name                    = "backend"
-  env                     = var.env
-  instance_type           = var.components["backend"].instance_type
-  port_no                 = var.components["backend"].port_no
-  port_no_of_nodeexporter = var.components["backend"].port_no_of_nodeexporter
+  Name          = "backend"
+  env           = var.env
+  instance_type = var.components["backend"].instance_type
+  port_no       = var.components["backend"].port_no
+  # port_no_of_nodeexporter = var.components["backend"].port_no_of_nodeexporter
 
 
 }
@@ -36,30 +36,30 @@ module "frontend" {
   zone_id    = data.aws_route53_zone.main.zone_id
   domain     = var.domain
   #vpc_security_group_ids = var.vpc_security_group_ids
-  Name                    = "frontend"
-  env                     = var.env
-  instance_type           = var.components["frontend"].instance_type
-  port_no                 = var.components["frontend"].port_no
-  port_no_of_nodeexporter = var.components["frontend"].port_no_of_nodeexporter
+  Name          = "frontend"
+  env           = var.env
+  instance_type = var.components["frontend"].instance_type
+  port_no       = var.components["frontend"].port_no
+  #port_no_of_nodeexporter = var.components["frontend"].port_no_of_nodeexporter
 
 
 }
 
-module "node123" {
-  depends_on = [module.backend]
-  source     = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
-  ami        = data.aws_ami.main.id
-  zone_id    = data.aws_route53_zone.main.zone_id
-  domain     = var.domain
-  #vpc_security_group_ids = var.vpc_security_group_ids
-  Name                    = "node123"
-  env                     = var.env
-  instance_type           = var.components["node123"]["instance_type"]
-  port_no                 = var.components["node123"]["port_no"]
-  port_no_of_nodeexporter = var.components["frontend"].port_no_of_nodeexporter
+# module "node123" {
+#   depends_on = [module.backend]
+#   source     = "git::https://github.com/ORG-NARESH/org-modules-EC2-R53.git"
+#   ami        = data.aws_ami.main.id
+#   zone_id    = data.aws_route53_zone.main.zone_id
+#   domain     = var.domain
+#   #vpc_security_group_ids = var.vpc_security_group_ids
+#   Name                    = "node123"
+#   env                     = var.env
+#   instance_type           = var.components["node123"]["instance_type"]
+#   port_no                 = var.components["node123"]["port_no"]
+#   port_no_of_nodeexporter = var.components["frontend"].port_no_of_nodeexporter
 
 
-}
+# }
 
 
 
