caccb0dcffee56ba8587daf7dd02ffdc3579c7ea
naresh <Naresh@gmail.com>
2025-04-26 19:54:18 +0530

    jenkins automation

--- variables.tf
+++ variables.tf
@@ -19,39 +19,39 @@ variable "env" {}
 
 variable "components" {
 
-  default = {
-    mysql = {
-      Name                    = "mysql"
-      port_no                 = "3306"
-      instance_type           = "t3.micro"
-      port_no_of_nodeexporter = "9100"
+  # default = {
+  #   mysql = {
+  #     Name                    = "mysql"
+  #     port_no                 = "3306"
+  #     instance_type           = "t3.micro"
+  #     port_no_of_nodeexporter = "9100"
 
-    }
-    backend = {
+  #   }
+  #   backend = {
 
-      Name                    = "backend"
-      port_no                 = "8080"
-      instance_type           = "t2.micro"
-      port_no_of_nodeexporter = "9100"
+  #     Name                    = "backend"
+  #     port_no                 = "8080"
+  #     instance_type           = "t2.micro"
+  #     port_no_of_nodeexporter = "9100"
 
-    }
-    frontend = {
+  #   }
+  #   frontend = {
 
-      Name                    = "frontend"
-      port_no                 = "80"
-      instance_type           = "t2.small"
-      port_no_of_nodeexporter = "9100"
+  #     Name                    = "frontend"
+  #     port_no                 = "80"
+  #     instance_type           = "t2.small"
+  #     port_no_of_nodeexporter = "9100"
 
-    }
-    node123 = {
+  #   }
+  #   node123 = {
 
-      Name                    = "node123"
-      port_no                 = "80"
-      instance_type           = "t2.small"
-      port_no_of_nodeexporter = "9100"
+  #     Name                    = "node123"
+  #     port_no                 = "80"
+  #     instance_type           = "t2.small"
+  #     port_no_of_nodeexporter = "9100"
 
-    }
+  #   }
 
-  }
+  # }
 }
 
