c94d31952cdb44ec4494d73cea2a792138b74192
naresh <Naresh@gmail.com>
2025-04-26 19:08:13 +0530

    jenkins automation

--- dev/dev.tfvars
+++ dev/dev.tfvars
@@ -3,31 +3,34 @@ env = "dev"
 components = {
 
   mysql = {
-    Name          = "mysql"
-    port_no       = "3306"
-    instance_type = "t3.medium"
+    Name                    = "mysql"
+    port_no                 = "3306"
+    instance_type           = "t3.medium"
+    port_no_of_nodeexporter = "9100"
 
   }
   backend = {
 
-    Name          = "backend"
-    port_no       = "8080"
-    instance_type = "t3.micro"
+    Name                    = "backend"
+    port_no                 = "8080"
+    instance_type           = "t3.micro"
+    port_no_of_nodeexporter = "9100"
 
   }
   frontend = {
 
-    Name          = "frontend"
-    port_no       = "80"
-    instance_type = "t3.small"
+    Name                    = "frontend"
+    port_no                 = "80"
+    instance_type           = "t3.small"
+    port_no_of_nodeexporter = "9100"
 
   }
   node123 = {
 
-    Name          = "node123"
-    port_no       = "80"
-    instance_type = "t2.small"
-    #port_no_of_nodeexporter = "9100"
+    Name                    = "node123"
+    port_no                 = "80"
+    instance_type           = "t2.small"
+    port_no_of_nodeexporter = "9100"
 
   }
 
--- variables.tf
+++ variables.tf
@@ -21,34 +21,34 @@ variable "components" {
 
   default = {
     mysql = {
-      Name          = "mysql"
-      port_no       = "3306"
-      instance_type = "t3.micro"
-      #port_no_of_nodeexporter = "9100"
+      Name                    = "mysql"
+      port_no                 = "3306"
+      instance_type           = "t3.micro"
+      port_no_of_nodeexporter = "9100"
 
     }
     backend = {
 
-      Name          = "backend"
-      port_no       = "8080"
-      instance_type = "t2.micro"
-      #port_no_of_nodeexporter = "9100"
+      Name                    = "backend"
+      port_no                 = "8080"
+      instance_type           = "t2.micro"
+      port_no_of_nodeexporter = "9100"
 
     }
     frontend = {
 
-      Name          = "frontend"
-      port_no       = "80"
-      instance_type = "t2.small"
-      #port_no_of_nodeexporter = "9100"
+      Name                    = "frontend"
+      port_no                 = "80"
+      instance_type           = "t2.small"
+      port_no_of_nodeexporter = "9100"
 
     }
     node123 = {
 
-      Name          = "node123"
-      port_no       = "80"
-      instance_type = "t2.small"
-      #port_no_of_nodeexporter = "9100"
+      Name                    = "node123"
+      port_no                 = "80"
+      instance_type           = "t2.small"
+      port_no_of_nodeexporter = "9100"
 
     }
 
