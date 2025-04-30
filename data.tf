c767f0f3d5eee481a786778146fc993ac03db8fa
naresh <Naresh@gmail.com>
2025-04-30 15:35:51 +0530

    jenkins automation

--- data.tf
+++ data.tf
@@ -17,7 +17,11 @@ data "aws_route53_zone" "main" {
 }
 
 data "vault_generic_secret" "main" {
-  path  = "expense-dev/SSH_CRED"
-  token = var.token
+  path = "expense-dev/SSH_CRED"
 
+
+}
+
+output "ssh" {
+  value = data.vault_generic_secret.main.data["SSH_CRED"]
 }
