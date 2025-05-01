e68f0153569fb87bd7dcee1c454d04a7e4706f2d
naresh <Naresh@gmail.com>
2025-05-01 19:37:34 +0530

    jenkins automation

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ module "mysql" {
   zone_id = data.aws_route53_zone.main.zone_id
   domain  = var.domain
   #vpc_security_group_ids  = [aws_security_group.main.id]  
-  Name                    = "mysql"
-  env                     = var.env
-  instance_type           = var.components["mysql"].instance_type
-  port_no                 = var.components["mysql"].port_no
-  port_no_of_nodeexporter = var.components["mysql"].port_no_of_nodeexporter
-  prometheus_server       = var.prometheus_server
-  Monitor                 = var.components["mysql"].Monitor
+  Name          = "mysql"
+  env           = var.env
+  instance_type = var.components["mysql"].instance_type
+  port_no       = var.components["mysql"].port_no
+  # port_no_of_nodeexporter = var.components["mysql"].port_no_of_nodeexporter
+  # prometheus_server       = var.prometheus_server
+  Monitor = var.components["mysql"].Monitor
 
 
 }
@@ -22,13 +22,13 @@ module "backend" {
   zone_id    = data.aws_route53_zone.main.zone_id
   domain     = var.domain
   #vpc_security_group_ids =  var.vpc_security_group_ids
-  Name                    = "backend"
-  env                     = var.env
-  instance_type           = var.components["backend"].instance_type
-  port_no                 = var.components["backend"].port_no
-  port_no_of_nodeexporter = var.components["backend"].port_no_of_nodeexporter
-  prometheus_server       = var.prometheus_server
-  Monitor                 = var.components["backend"].Monitor
+  Name          = "backend"
+  env           = var.env
+  instance_type = var.components["backend"].instance_type
+  port_no       = var.components["backend"].port_no
+  # port_no_of_nodeexporter = var.components["backend"].port_no_of_nodeexporter
+  # prometheus_server       = var.prometheus_server
+  Monitor = var.components["backend"].Monitor
 
 
 
@@ -41,13 +41,13 @@ module "frontend" {
   zone_id    = data.aws_route53_zone.main.zone_id
   domain     = var.domain
   #vpc_security_group_ids = var.vpc_security_group_ids
-  Name                    = "frontend"
-  env                     = var.env
-  instance_type           = var.components["frontend"].instance_type
-  port_no                 = var.components["frontend"].port_no
-  port_no_of_nodeexporter = var.components["frontend"].port_no_of_nodeexporter
-  prometheus_server       = var.prometheus_server
-  Monitor                 = var.components["frontend"].Monitor
+  Name          = "frontend"
+  env           = var.env
+  instance_type = var.components["frontend"].instance_type
+  port_no       = var.components["frontend"].port_no
+  # port_no_of_nodeexporter = var.components["frontend"].port_no_of_nodeexporter
+  # prometheus_server       = var.prometheus_server
+  Monitor = var.components["frontend"].Monitor
 
 
 }
